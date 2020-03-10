001a78112ec98dc8a87e75e91300738becb8a4ba
Craig A Stueber <58899336+CraigStueber@users.noreply.github.com>
2020-03-10 01:10:38 -0400

    Add files via upload

---
+++ eberron.js
@@ -0,0 +1,10 @@
+
+function showstuff(id){
+	const i = document.getElementById(id);
+	var displayi = i.style.display;
+	if(displayi == 'block'){
+		i.style.display = 'none'
+	}else{
+		i.style.display = 'block'
+	}
+}
